2f27904a7d310202a38848ec0fba9d522cf04dd6
Alain Armand <mandalarian@Alains-MacBook-Pro.local>
2020-03-23 17:54:30 -0400

    works in browser

--- App.js
+++ App.js
@@ -1,19 +1,89 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Amplify from '@aws-amplify/core'
+import config from './aws-exports'
+Amplify.configure(config)
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import React, { useState, useEffect } from 'react'
+import { Text, View, TextInput, Button } from 'react-native'
+// import the datastore api
+import { DataStore } from '@aws-amplify/datastore'
+// import the message model
+import { Message} from './src/models'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// create some initial state
+// type is: `const Message: PersistentModelConstructor<any>`
+const initialState = { color: 'black', title: '' }
+
+// create app component with form state and messages
+
+function App(){
+    const [formState, updateFormState] = useState(initialState)
+    const [messages, updateMessages] = useState([])
+
+    // useEffect hook to get messages after component loads
+
+    useEffect(() => {
+      fetchMessages()
+      // use an observable to subcribe to new message then unsubscribe when new message arrives
+      // call `fetchMessages` again then unsubscribe.
+      const subscription = DataStore.observe(Message).subscribe(() => fetchMessages())
+      return () => subscription.unsubscribe()
+    })
+    // update form state as user types
+    function onChangeText(key, value) {
+      updateFormState({ ...formState, [key]: value })
+    }
+
+    // function to query datastore by calling DataStore.query and passing in
+    //  the message model then call `update messages` to update the messages array
+    async function fetchMessages() {
+      const messages = await DataStore.query(Message)
+      updateMessages(messages)
+    }
+    // check if formState title is not empty, if not, save with DataStore Api
+    async function createMessage() {
+      if (!formState.title) return
+      await DataStore.save(new Message({ ...formState }))
+      // clear form by returning to initial state
+      updateFormState(initialState)
+    } 
+    
+    return (
+      <View style={container}>
+        <Text style={heading}>Real Time Message Board</Text>
+        <TextInput
+          onChangeText={v => onChangeText('title', v)}
+          placeholder='Message title'
+          value={formState.title}
+          style={input}
+        />
+        <TextInput
+          onChangeText={v => onChangeText('color', v)}
+          placeholder='Message color'
+          value={formState.color}
+          style={input}
+          autoCapitalize='none'
+        />
+        <Text>Color: <Text style={{fontWeight: 'bold', color: formState.color}}>{formState.color}</Text></Text>
+        <Button onPress={createMessage} title='Create Message' />
+        {
+          messages.map(message => (
+            <View key={message.id} style={{...messageStyle, backgroundColor: message.color}}>
+              <View style={messageBg}>
+                <Text style={messageTitle}>{message.title}</Text>
+              </View>
+            </View>
+          ))
+        }
+      </View>
+    )
+  }
+  
+  const container = { padding: 20, paddingTop: 80 }
+  const input = { marginBottom: 10, padding: 7, backgroundColor: '#ddd' }
+  const heading = { fontWeight: 'normal', fontSize: 40 }
+  const messageBg = { backgroundColor: 'white' }
+  const messageStyle = { padding: 20, marginTop: 7, borderRadius: 4 }
+  const messageTitle = { margin: 0, padding: 9, fontSize: 20  }
+  
+  export default App
+  
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Egghead.io/Amplify Datastore
+# Egghead.io/Building Offline-first Cross-platform Apps with Expo and Amplify DataStore     
 
 ## https://aws.amazon.com/about-aws/whats-new/2019/12/introducing-amplify-datastore/
 
@@ -71,3 +71,88 @@ GraphQL API KEY: da2-gbcxu47jwjggxns53ne442rna4
 
 See gist: <https://gist.github.com/dabit3/184e875c151335c00336396c99f9ef06>
 
+```js
+import Amplify from '@aws-amplify/core'
+import config from './aws-exports'
+Amplify.configure(config)
+
+import React, { useState, useEffect } from 'react'
+import { Text, View, TextInput, Button } from 'react-native'
+// import the datastore api
+import { DataStore } from '@aws-amplify/datastore'
+// import the message model
+import { Message} from './src/models'
+
+// create some initial state
+// type is: `const Message: PersistentModelConstructor<any>`
+const initialState = { color: 'black', title: '' }
+
+// create app component with form state and messages
+
+function App(){
+    const [formState, updateFormState] = useState(initialState)
+    const [messages, updateMessages] = useState([])
+
+    // useEffect hook to get messages after component loads
+
+    useEffect(() => {
+      fetchMessages()
+      // use an observable to subcribe to new message then unsubscribe when new message arrives
+      // call `fetchMessages` again then unsubscribe.
+      const subscription = DataStore.observe(Message).subscribe(() => fetchMessages())
+      return () => subscription.unsubscribe()
+    })
+    // update form state as user types
+    function onChangeText(key, value) {
+      updateFormState({ ...formState, [key]: value })
+    }
+
+    // function to query datastore by calling DataStore.query and passing in
+    //  the message model then call `update messages` to update the messages array
+    async function fetchMessages() {
+      const messages = await DataStore.query(Message)
+      updateMessages(messages)
+    }
+    // check if formState title is not empty, if not, save with DataStore Api
+    async function createMessage() {
+      if (!formState.title) return
+      await DataStore.save(new Message({ ...formState }))
+      // clear form by returning to initial state
+      updateFormState(initialState)
+    } 
+    
+    return (
+      <View style={container}>
+        <Text style={heading}>Real Time Message Board</Text>
+        <TextInput
+          onChangeText={v => onChangeText('title', v)}
+          placeholder='Message title'
+          value={formState.title}
+          style={input}
+        />
+        <TextInput
+          onChangeText={v => onChangeText('color', v)}
+          placeholder='Message color'
+          value={formState.color}
+          style={input}
+          autoCapitalize='none'
+        />
+        <Text>Color: <Text style={{fontWeight: 'bold', color: formState.color}}>{formState.color}</Text></Text>
+        <Button onPress={createMessage} title='Create Message' />
+        {
+          messages.map(message => (
+            <View key={message.id} style={{...messageStyle, backgroundColor: message.color}}>
+              <View style={messageBg}>
+                <Text style={messageTitle}>{message.title}</Text>
+              </View>
+            </View>
+          ))
+        }
+      </View>
+    )
+  }
+```
+
+## Try it
+
+Run `expo start` and click `open in browser` and `open in simulator` to see them working together.
